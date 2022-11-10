480c2d8a4a5d985f1387819f5314f5d28b2c53a6
Darya Kulikova <ivolgadaria@yandex.ru>
2022-11-10 08:29:30 +0300

    Задание 'День программиста'

---
+++ src/Praktikum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Praktikum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите год:");
+        int year = scanner.nextInt();
+
+        if (isLeapYear(year)) {
+            System.out.println("12.09." + year);
+        }
+        else System.out.println("13.09." + year);
+    }
+
+    public static boolean isLeapYear(int year) {
+        boolean isLeap = false;
+        if(year%4 == 0)
+        {
+            isLeap = true;
+        }
+        return isLeap;
+    }
+} 
